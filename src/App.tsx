aadda78ce7ed0c68dd419f1ecd1e7e1caf028593
John McNeill <jh_mcneill@yahoo.ca>
2020-03-23 20:16:04 -0400

    added themes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
 import React, { useState, useEffect } from "react";
 import { Routes } from "./Routes";
 import { setAccessToken } from "./accessToken";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import {
+  createMuiTheme,
+  ThemeProvider,
+  FormControlLabel,
+  Checkbox
+} from "@material-ui/core";
+
+const darkTheme = createMuiTheme({
+  palette: {
+    type: "dark"
+  }
+});
+
+const lightTheme = createMuiTheme({
+  palette: {
+    type: "light"
+  }
+});
 
 const App: React.FC = () => {
   const [loading, setLoading] = useState(true);
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setChecked(event.target.checked);
+  };
 
   useEffect(() => {
     fetch("http://localhost:4000/refresh_token", {
@@ -20,7 +44,18 @@ const App: React.FC = () => {
     return <div>loading...</div>;
   }
 
-  return <Routes />;
+  return (
+    <>
+      <ThemeProvider theme={checked ? lightTheme : darkTheme}>
+        <CssBaseline />
+        <FormControlLabel
+          control={<Checkbox checked={checked} onChange={handleChange} />}
+          label="Theme"
+        />
+        <Routes />
+      </ThemeProvider>
+    </>
+  );
 };
 
 export default App;
